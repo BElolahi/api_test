661e4cd377295c7b7d0bc4ffcd7f27288815eb9c
BElolahi <Belal.Elolahi@optimumpartners.co>
2022-06-07 15:14:29 +0300

    add

--- cypress.config.js
+++ cypress.config.js
@@ -3,17 +3,16 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
  
   setupNodeEvents(on, config) {
-    return config ;
+    return config ; // here you can add ur plugin folder. 
   },
   component: {
     viewportWidth: 500,
     viewportHeight: 500
   },
-  // Command timeout overridden for E2E tests
   e2e: {
+    projectId: "pj57oz",
     defaultCommandTimeout: 10000,
     specPattern:"**/*.cy.{js,jsx,ts,tsx}",
-    projectId: "pj57oz",
     fixturesFolder: "./cypress/fixtures",
     supportFile: "./cypress/support/e2e.{js,jsx,ts,tsx}",
     chromeWebSecurity: false,
